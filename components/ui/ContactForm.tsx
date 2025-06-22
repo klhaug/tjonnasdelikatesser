bd1b46f2a722ff3a08f2651644c356a930da16a4
klhaug <kristian@snougle.no>
2025-06-22 09:22:37 +0200

    pushing contactform to test if CORS is only issue

--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -1,23 +1,43 @@
 'use client'
 
+import React from "react";
+
 
 
 export default function ContactForm() {
-//     const formRef = useRef(null);
 
-//   useEffect(() => {
-//     if (formRef.current) {
-//       formRef.current.reset();
-//     }
-//   }, []);
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+      formData.append("access_key", "f9401c4c-5bce-4ddb-b411-5109d802e289");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
   return (
-    <form action="https://api.web3forms.com/submit" method="POST" id='contactform' className='px-6 flex flex-col w-full max-w-[576px] gap-4'>
+    <form onSubmit={onSubmit} id='contactform' className='px-6 flex flex-col w-full max-w-[576px] gap-4'>
         <input type="hidden" name="access_key" value="f9401c4c-5bce-4ddb-b411-5109d802e289"/>
         <label htmlFor="name">
-            <input className='h-11 px-4 w-full rounded-md bg-grey-100  focus:outline-yellow-350' required id='name' type='text' placeholder='Navn'/>
+            <input className='h-11 px-4 w-full rounded-md bg-grey-100  focus:outline-yellow-350' name="name" required id='name' type='text' placeholder='Navn'/>
         </label>
         <label htmlFor="email">
-            <input className='h-11 px-4 w-full rounded-md bg-grey-100  focus:outline-yellow-350' required id='email' type='email' placeholder='E-post'/>
+            <input className='h-11 px-4 w-full rounded-md bg-grey-100  focus:outline-yellow-350' name='name' required id='email' type='email' placeholder='E-post'/>
         </label>
         <fieldset className='px-4 '>
             <legend className='inline-block'>Hva gjelder det?</legend>
@@ -31,6 +51,7 @@ export default function ContactForm() {
         <label className='w-full' htmlFor="message"><textarea id='message' name='message' className='bg-grey-100 w-full p-4 rounded-md focus:outline-yellow-350' rows={8} placeholder='Melding'/></label>
         <input type="hidden" name="redirect" value="https://web3forms.com/success"/>
         <button className='text-base h-[44px] focus:bg-yellow-350 focus:outline-red-500 bg-yellow-300 flex justify-center items-center text-nowrap w-full hover:bg-yellow-350 hover:cursor-pointer rounded-lg pl-6 pr-6' type='submit'>Send</button>
+        <span>{result}</span>
     </form>
   )
 }
