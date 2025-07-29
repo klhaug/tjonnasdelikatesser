dcdd87339632612c0685ac1c0fb0dde842ddba87
klhaug <kristian@snougle.no>
2025-07-29 17:35:27 +0200

    added priority hero img

--- components/sections/DesktopHero.tsx
+++ components/sections/DesktopHero.tsx
@@ -8,7 +8,7 @@ export default function DesktopHero() {
     <div className='h-[700px] hidden bg-gradient-image lg:flex flex-col items-center justify-end'>
       <div className='py-20 flex flex-col items-center gap-4'>
         <div className='flex flex-col max-w-2/3 gap-4'>
-          <Image src="/images/Tjønnås_Logo-hvit 1.svg" alt='tjonnas icon' height={90} width={600} />
+          <Image priority={true} src="/images/Tjønnås_Logo-hvit 1.svg" alt='tjonnas icon' height={90} width={600} />
           <Text variant='primary' content="Delikatesser laget med lidenskap, lokale råvarer og respekt for tradisjon." extraStyling='text-white text-center' as={'p'} />
         </div>
         <div className='flex gap-8'>
