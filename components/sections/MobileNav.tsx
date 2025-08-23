9ba568938ef91bd2c6bdac603159a672d4e126dc
klhaug <kristian@snougle.no>
2025-08-23 13:21:24 +0200

    fixed unnecessary import that caused vercel build error

--- components/sections/MobileNav.tsx
+++ components/sections/MobileNav.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import React, { useRef, useState } from 'react'
-import Text from '../ui/Text'
 import Link from 'next/link';
 
 export default function MobileNav() {
