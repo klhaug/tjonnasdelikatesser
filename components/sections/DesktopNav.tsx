b9289e4ef3de440b6ab0d2f371b6ba28554b749f
klhaug <kristian@snougle.no>
2025-08-23 11:12:55 +0200

    updated logo aspect ratio to improve lighthouse best practises

--- components/sections/DesktopNav.tsx
+++ components/sections/DesktopNav.tsx
@@ -8,7 +8,7 @@ export default function DesktopNav() {
     <div className='hidden lg:block border-b border-b-grey-100 w-full'>
        <nav className='text-nowrap m-auto relative flex justify-between items-center max-w-[1440px] md:px-12 lg:px-22 py-12 '>
          <Link href="/">
-                <Image src="/images/Tjønnås_Ikon-oker.png" className='h-[44px] w-[38px]' height={34} width={28} alt='icon'/>
+                <Image src="/images/Tjønnås_Ikon-oker.png" className='h-[44px] w-[35px]' height={34} width={28} alt='icon'/>
             </Link>
               <ul className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 gap-8 lg:gap-12 flex items-start justify-self-center  justify-between'>
                 <li>
