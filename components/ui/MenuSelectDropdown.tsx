c5c7cac7ef027045bc410b34eb4c1390cd0a0326
klhaug <kristian@snougle.no>
2025-08-29 15:54:32 +0200

    changed something... dunno

--- components/ui/MenuSelectDropdown.tsx
+++ components/ui/MenuSelectDropdown.tsx
@@ -15,7 +15,7 @@ const borderVariantsOpen = {
 
 const menuBasedOnState = {
   tjonnasdelikatesser: "Tjønnås Delikatesser",
-  catering: "Catering",
+  catering: "Catering / Selskapsmeny",
   norvald: "Norvald"
 }
 
@@ -54,7 +54,7 @@ export default function MenuSelectDropdown({selectedMenu, setSelectedMenu}: {sel
         <div className={`${isOpen ? "flex" : 'hidden'} absolute flex-col top-18 rounded-b-md w-full shadow-xl  bg-white border-2 border-grey-100`}>
           <button onClick={(e) => handleOnClick(e)} value="tjonnasdelikatesser" className='hover:bg-yellow-300 hover:cursor-pointer p-4'>Tjønnas Delikatesser</button>
           <button onClick={(e) => handleOnClick(e)} value="norvald" className='hover:bg-blue-500 hover:cursor-pointer p-4'>Norvald</button>
-          <button onClick={(e) => handleOnClick(e)} value="catering" className='hover:bg-green-500 hover:cursor-pointer p-4 hover:text-white rounded-b-md'>Catering</button>
+          <button onClick={(e) => handleOnClick(e)} value="catering" className='hover:bg-green-500 hover:cursor-pointer p-4 hover:text-white rounded-b-md'>Catering / Selskapsmeny</button>
         </div>
     </div>
   )
